dc1db15f4c8417537d3bbe32a831365fcbe3177e
David Duggins <weatheredwatcher@gmail.com>
2010-04-27 11:28:05 -0400

    Started the Question class

--- javascript/quizModule.js
+++ javascript/quizModule.js
@@ -6,3 +6,22 @@
 	Created: April 27th, 2010
 	Last Modified: April 27th, 2010 DWD
 */
+
+function Question(questionType, questionText, questionChoices, questionAnswers, answers){
+	
+	this.questionType = questionType;
+	this.questionText = questionText;
+	this.questionChoices = questionChoices.split(" ");
+	this.questionAnswers = questionAnswers.split(" ");
+	this.answers = answers;
+}
+
+Question.prototype.setAnswer(selected) = function {
+	this.selected = selected;
+	if (this.questionType == 1){ //type 1 = True/False Question Logic
+		
+	}
+	if (this.questionType == 2){ //type 2 = multiple choice/multiple answer logic
+		this.answers.push(this.selected);
+	}
+}
