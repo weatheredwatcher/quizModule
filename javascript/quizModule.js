bd7d80e61bb996f52ad64c653daa2150157d8e6c
David Duggins <weatheredwatcher@gmail.com>
2010-04-27 14:52:48 -0400

    added a few methods to the Question class

--- javascript/quizModule.js
+++ javascript/quizModule.js
@@ -11,17 +11,28 @@ function Question(questionType, questionText, questionChoices, questionAnswers, 
 	
 	this.questionType = questionType;
 	this.questionText = questionText;
-	this.questionChoices = questionChoices.split(" ");
-	this.questionAnswers = questionAnswers.split(" ");
+	this.questionChoices = questionChoices.split(",");
+	this.questionAnswers = questionAnswers.split(",");
 	this.answers = answers;
+	
+
 }
 
-Question.prototype.setAnswer(selected) = function {
+
+Question.prototype.setAnswer = function(selected) {
 	this.selected = selected;
 	if (this.questionType == 1){ //type 1 = True/False Question Logic
-		
+		this.answer = this.selected;
 	}
 	if (this.questionType == 2){ //type 2 = multiple choice/multiple answer logic
 		this.answers.push(this.selected);
 	}
 }
+
+Question.prototype.displayQuestion = function(){
+	
+	alert(this.questionText);
+	
+}
+
+var myQuestion = new Question(2,"What was the question again?", "Will\,while whistling\, went to the store.,You,Us,Them", "Me,Us,Them");
