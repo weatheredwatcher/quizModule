470d54bfe15e78c5e9394fac99f76624e898e074
David Duggins <weatheredwatcher@gmail.com>
2010-04-27 16:35:24 -0400

    Added the logic to check multiple choice/multiple answer questions

--- javascript/quizModule.js
+++ javascript/quizModule.js
@@ -6,33 +6,55 @@
 	Created: April 27th, 2010
 	Last Modified: April 27th, 2010 DWD
 */
+Array.prototype.compare = function(testArr) {
+    if (this.length != testArr.length) return false;
+    for (var i = 0; i < testArr.length; i++) {
+        if (this[i].compare) { 
+            if (!this[i].compare(testArr[i])) return false;
+        }
+        if (this[i] !== testArr[i]) return false;
+    }
+    return true;
+}
 
 function Question(questionType, questionText, questionChoices, questionAnswers, answers){
 	
 	this.questionType = questionType;
 	this.questionText = questionText;
-	this.questionChoices = questionChoices.split(",");
+	this.questionChoices = questionChoices.split("|");
 	this.questionAnswers = questionAnswers.split(",");
 	this.answers = answers;
 	
 
 }
 
+Question.prototype.checkAnswer = function(){
+	var myAnswers = [];
+	for(var i=0; i<document.answers.answer.length; ++i){
+	if(document.answers.answer[i].checked){
+	
+		myAnswers.push(i);
+	}
+	}
+	
+	if(this.questionAnswers.toString() == myAnswers.toString()){
+		alert("true");
+	}else {
+		alert("false");
+	}
+	//alert("Guesses:"+ myAnswers + "Answers:" + this.questionAnswers);
 
-Question.prototype.setAnswer = function(selected) {
-	this.selected = selected;
-	if (this.questionType == 1){ //type 1 = True/False Question Logic
-		this.answer = this.selected;
-	}
-	if (this.questionType == 2){ //type 2 = multiple choice/multiple answer logic
-		this.answers.push(this.selected);
-	}
 }
-
 Question.prototype.displayQuestion = function(){
-	
-	alert(this.questionText);
+		myHTMLOutput = "<h4>" + this.questionText + "</h4><form name='answers'>";
+		for ( var i=0, len=this.questionChoices.length; i<len; ++i ){
+		  myHTMLOutput += '<input type="checkbox" name="answer" value="' + i + '" />' + this.questionChoices[i] + "<br />";
+		 
+		}
+		 myHTMLOutput += '<input type="button" value="Check" onClick="myQuestion.checkAnswer()" />';
+		document.write(myHTMLOutput);
 	
 }
 
-var myQuestion = new Question(2,"What was the question again?", "Will\,while whistling\, went to the store.,You,Us,Them", "Me,Us,Them");
+var myQuestion = new Question(2,"What was the question again?", "My,What Big teeth you have!|You|Us|Them", "0,2,3");
+myQuestion.displayQuestion();
