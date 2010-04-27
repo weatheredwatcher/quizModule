163f078eedf8c62f1cc17ddba87d8fa57ffaeafa
David Duggins <weatheredwatcher@gmail.com>
2010-04-27 11:05:41 -0400

    creating structure and basic files

---
+++ javascript/quizModule.js
@@ -0,0 +1,8 @@
+/*
+	quizModule.js
+	Author: David Duggins
+	Email: weatheredwatcher@gmail.com
+	Project Location: http://github.com/weatheredwatcher/quizModule
+	Created: April 27th, 2010
+	Last Modified: April 27th, 2010 DWD
+*/
